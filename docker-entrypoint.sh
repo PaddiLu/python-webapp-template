98f4d73710007021047b23f7f516e96df650cb5e
Patrick Lübcke <patrickluebcke@gmail.com>
2026-06-03 20:20:27 +0200

    feat(docker): read Flask secret key from file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-# Randomize FLASK_SECRET_KEY if unset
+# Read FLASK_SECRET_KEY from FLASK_SECRET_KEY_FILE or randomize FLASK_SECRET_KEY if unset
 if [ -z $FLASK_SECRET_KEY ]; then
-	export FLASK_SECRET_KEY=$(hexdump -vn16 -e'4/4 "%08X" 1 "\n"' /dev/urandom)
+	if [[ -f "$FLASK_SECRET_KEY_FILE" && -r "$FLASK_SECRET_KEY_FILE" ]]
+	then
+		export FLASK_SECRET_KEY=$(cat "$FLASK_SECRET_KEY_FILE")
+	else
+		[ -n "$FLASK_SECRET_KEY_FILE" ] && echo -e "\033[1;33mWARNING:\033[22m Could not read secret key from file: '$FLASK_SECRET_KEY_FILE'\033[0m" >&2
+		export FLASK_SECRET_KEY=$(hexdump -vn16 -e'4/4 "%08X" 1 "\n"' /dev/urandom)
+	fi
 fi
 
 # Execute command (as non-root user)
